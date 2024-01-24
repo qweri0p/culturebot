9ce15bebd58736792777899f6a8ff90e3237c985
qweri0p <71005650+qweri0p@users.noreply.github.com>
2024-01-24 17:17:58 +0100

    i NEED a better fix than this this stinks!!!

--- index.ts
+++ index.ts
@@ -53,31 +53,25 @@ client.once(Events.ClientReady, c => {
 });
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	await interaction.deferReply()
+	if (interaction.isChatInputCommand()) {
+		await interaction.deferReply()
 
-	const command = commands.get(interaction.commandName);
+		const command = commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.editReply({ content: 'There was an error while executing this command!'});
 		}
 	}
-});
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (interaction.isChatInputCommand()) {
-	} else if (interaction.isAutocomplete()) {
+	else if (interaction.isAutocomplete()) {
 		const command = commands.get(interaction.commandName);
 
 		if (!command) {
